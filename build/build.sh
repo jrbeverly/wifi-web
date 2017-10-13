bd3a8e7495ef0eddaccb35e90e3fdf9c52efb57d
jrbeverly <jrbeverly@live.com>
2017-10-12 22:46:27 -0400

    Setting up the build process

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+#
+# Variables
+#
 DIR=$(dirname "$(readlink -f "$0")")
 DIR_ROOT=$(dirname "$DIR")
 DIR_SRC="$DIR_ROOT/src"
 DIR_OUT="$DIR_ROOT/out"
+
+# Main
+echo "Nothing to do!"
--- build/package.sh
+++ build/package.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+command -v zip >/dev/null 2>&1 || { echo >&2 "The script requires 'zip' but it's not installed.  Aborting."; exit 1; }
+
+#
+# Variables
+#
 DIR=$(dirname "$(readlink -f "$0")")
 DIR_ROOT=$(dirname "$DIR")
 DIR_SRC="$DIR_ROOT/src"
 DIR_OUT="$DIR_ROOT/out"
 
+VERSION=$(cat "$DIR/VERSION")
+RELEASE="githooks-$VERSION.zip"
+
+#
 # Main
-mkdir -p "$DIR_OUT" 
-cp -R "$DIR_SRC/*" "$DIR_OUT/" 
+#
+rm -rf "${DIR_OUT:?}"/*
+mkdir -p "$DIR_OUT"
+zip -r "$DIR_OUT/$RELEASE"  "$DIR_SRC/*"
