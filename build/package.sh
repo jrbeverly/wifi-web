6164ecbf2d52f3df17d5d2f01df3f14a1713d50e
jrbeverly <jrbeverly@live.com>
2018-05-29 18:04:59 -0400

    docs: Remove extra space in comment

--- build/package.sh
+++ build/package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-command -v zip >/dev/null 2>&1 || { echo >&2 "The script requires 'zip' but it's not installed.  Aborting."; exit 1; }
+command -v zip >/dev/null 2>&1 || { echo >&2 "The script requires 'zip' but it's not installed. Aborting."; exit 1; }
 
 #
 # Variables
